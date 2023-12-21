c478cc58edde9d6420f42b73945d423a649dfad4
Nec2005 <necatiunal2005@gmail.com>
2023-12-21 11:42:56 +0000

    fix repo

--- tutorials/basic/publishing-outputs/bump-timestamp-file.sh
+++ tutorials/basic/publishing-outputs/bump-timestamp-file.sh
@@ -8,8 +8,8 @@ git clone resource-gist updated-gist
 cd updated-gist
 date > bumpme
 
-git config --global user.email "nobody@concourse-ci.org"
-git config --global user.name "Concourse"
+git config --global user.email "necatiunal2005@gmail.com"
+git config --global user.name "Nec2005"
 
 git add .
 git commit -m "Bumped date"
